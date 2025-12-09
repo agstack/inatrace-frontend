617dcabacdffdc789708ee23163a7d62f8bcabfb
Pece Adjievski <pece.adzievski@sunesis.si>
2025-12-09 13:31:09 +0100

    Updated whisp iframe URL

--- src/app/company/company-farmers/open-plot-details-externally-modal/open-plot-details-externally-modal.component.ts
+++ src/app/company/company-farmers/open-plot-details-externally-modal/open-plot-details-externally-modal.component.ts
@@ -23,7 +23,7 @@ export class OpenPlotDetailsExternallyModalComponent implements OnInit {
     
     if (this.geoId) {
       const layers = '%7B%22CocoaETH%22%3A%7B%22opacity%22%3A1%7D%2C%22JRCForestMask%22%3A%7B%22opacity%22%3A1%7D%2C%22OilPalmFDAP%22%3A%7B%22opacity%22%3A1%7D%7D';
-      const map = '%7B%22center%22%3A%7B%22lat%22%3A-3.0695179175276217%2C%22lng%22%3A103.97290617227554%7D%2C%22zoom%22%3A15%2C%22mapType%22%3A%22satellite%22%7D';
+      const map = '%7B%22zoom%22%3A15%2C%22mapType%22%3A%22satellite%22%7D';
       const scripts = '%7B%22WhispSummary%22%3A%7B%22dateRange%22%3A%5Bnull%2Cnull%5D%2C%22threshold%22%3Anull%2C%22extraThreshold%22%3Anull%2C%22multiCriteriaOption%22%3Anull%2C%22multiCriteriaOptionClass%22%3Anull%7D%7D';
       this.externalUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://whisp.earthmap.org/?aoi=WHISP&boundary&geoId=${this.geoId}&layers=${layers}&map=${map}&scripts=${scripts}&statisticsOpen=true`);
     }
